94fb2751e4439baabcf8c2515103b1b0496b9e71
Al <xochipillitx@gmail.com>
2018-02-25 23:02:40 -0600

    Tiled JSON and tmx files created for test map.

---
+++ angels-folder/tiled-maps/testTilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="testTilesheet" tilewidth="48" tileheight="48" tilecount="100" columns="10">
+ <image source="../../assets/sprites/tilesheets/testTilesheet.png" width="480" height="480"/>
+</tileset>
