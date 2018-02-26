8492bb9bbe9ea3afcfc02c4de4891ca7fa7413e0
Al <xochipillitx@gmail.com>
2018-02-26 00:53:18 -0600

    Fixed asset link typo

--- js/load.js
+++ js/load.js
@@ -12,7 +12,7 @@ loadState.prototype = {
 
     //Loading all assests
     //Sprites
-    this.game.load.spritesheet('player', 'assets/sprites/spritesheets/', 48, 48);
+    this.game.load.spritesheet('player', 'assets/sprites/spritesheets/playerSheet.png', 48, 48);
     //Maps
     // First load the tilemap JSON, and then the corresponding tileset image
     this.game.load.tilemap('testMap', 'assets/maps/testMap.JSON', null, Phaser.Tilemap.TILED_JSON);
